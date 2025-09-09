b97aabe3e4feabf3a59f03bcd6604c113a33f310
steven <steven.bouobeid@eulerds.com>
2025-09-09 16:21:30 +0300

    fix-add-authentication-route

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,16 @@ import { PORT } from "./config/env";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db";
 import errorMiddleware from "./middlewares/error.middleware";
+import authRouter from "./routes/auth.route";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Routes
+app.use("/auth", authRouter);
 
 app.use(errorMiddleware);
 
