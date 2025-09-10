6e96e422326b2688804c30b37fd1fcc99daccc85
steven <steven.bouobeid@eulerds.com>
2025-09-10 15:29:15 +0300

    fix-allow-cors-origin

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,20 @@ import { PORT } from "./config/env";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./config/db";
 import errorMiddleware from "./middlewares/error.middleware";
+import corsMiddleware from "./middlewares/cors.middleware";
 import authRouter from "./routes/auth.route";
 import categoryRouter from "./routes/category.route";
 import sectionRouter from "./routes/section.route";
 import serviceRouter from "./routes/service.route";
 import supplierRouter from "./routes/supplier.route";
+import cors from "cors";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(corsMiddleware);
 
 // Routes
 app.use("/auth", authRouter);
@@ -21,6 +24,14 @@ app.use("/categories", categoryRouter);
 app.use("/sections", sectionRouter);
 app.use("/services", serviceRouter);
 app.use("/suppliers", supplierRouter);
+
+// CORS
+app.use(
+  cors({
+    origin: "http://localhost:5173", // frontend URL
+    credentials: true, // if you need cookies/auth
+  })
+);
 
 app.use(errorMiddleware);
 
